d225c679eb0cac0214976a56c56b2395e840b578
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-09-13 09:37:54 +0530

    feat: 04 conversion completed

---
+++ Playground/03 Strings.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+var greeting = "Hello, playground !"
+var username: String = "Prince"
+var age = 44
+
+var myFinalNotice = "user's age is  \(age) and he lives in Canada"
+var result = greeting + " " + username
+print(result)
+print(myFinalNotice)
+//print(username) // Error username not initialized
+
+print(greeting.contains("o,"))
+print(greeting.count)
+print(greeting.isEmpty)
+
+print(greeting)
+print(greeting.removeLast())
+print(greeting)
+print(greeting.split(separator: " "))
