f23423ad5ef0defb462a9dc3f0d2749684232886
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-09-13 09:20:37 +0530

    feat: 01 variables and constants completed

---
+++ Playground/01 Variables and constants.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var username = "Prince"
+let accountId = 12345
+print(username)
+print(accountId)
+username="Hitesh Choudhary"
+//accountId = 5
+print(username)
+print(accountId)
+
+var numberOfLoginsInADay: Int = 34
+print(numberOfLoginsInADay)
+
+var name, email, address: String
+//Can't use before initialization
+//print(name)
+//print(email)
+//print(address)
