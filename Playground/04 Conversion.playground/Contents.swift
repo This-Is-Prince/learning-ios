e54e8e8fd97ef24c0e2abcf8f47e39b2c4b73c02
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-09-13 12:31:38 +0530

    feat: 04 conversion completed

---
+++ Playground/04 Conversion.playground/Contents.swift
@@ -0,0 +1,12 @@
+print("HC")
+
+let googleStock = 12.5
+print(googleStock)
+let userStockBuy:Int = 1 + Int(googleStock)
+print(userStockBuy)
+
+let userAge = "55a"
+
+let userAgeInSystem = Int(userAge)
+
+let storedValue = (userAgeInSystem ?? 0) + 5
